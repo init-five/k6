b63dd947bb65f6f9aed916b263b15273b9f15987
init-five <subhanihsan3@gmail.com>
2023-12-08 15:19:07 +0500

    Stress Test

---
+++ stress_test.js
@@ -0,0 +1,32 @@
+import { sleep } from "k6";
+import http from "k6/http";
+
+export const options = {
+	// ramp-up time
+	stages: [
+		// goees from 0 to 1000 users
+		{
+			duration: "5m",
+			target: 1000,
+		},
+		// stay on site with 1000 users
+		{
+			duration: "30m",
+			target: 1000,
+		},
+		// go down from 1000 to 0 user in 5minutes
+		{
+			duration: "5m",
+			target: 0,
+		},
+	],
+};
+
+export default function () {
+	http.get("https://test.k6.io");
+	sleep(2);
+	http.get("https://test.k6.io/contacts.php");
+	sleep(2);
+	http.get("https://test.k6.io/news.php");
+	sleep(2);
+}
