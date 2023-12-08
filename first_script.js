2f776d10ed5f481ef61935c9a5b9ffd3d8363e56
init-five <subhanihsan3@gmail.com>
2023-12-08 12:12:01 +0500

    First 10 users

--- first_script.js
+++ first_script.js
@@ -1,5 +1,15 @@
 import http from "k6/http";
+import { sleep } from "k6";
+
+//virutal users
+export const options = {
+	vus: 10,
+	duration: "10s",
+};
 
 export default function () {
 	http.get("https://test.k6.io");
+
+	//sleep
+	sleep(1);
 }
